53b92c8b1cb35dc88d95e9a9fc169acfbf12c557
“uliana” <avdeewa.ul@yandex.ru>
2018-03-01 15:37:36 +0300

    another output table

--- examples/first-step/qualifiers.php
+++ examples/first-step/qualifiers.php
@@ -18,22 +18,19 @@ $TVSession = new TV\Session(); // создание сессии: https://topviso
 $projectId = 2121417; // введите id своего проекта
 
 try{
-	echo "<b>Регион: \"ключевое слово\" - частота</b><br>\n"; // Вывод шапки таблицы
+	echo "<b>====== Санкт-Петербург / Москва / Россия</b><br>\n"; // вывод шапки таблицы
 	
 	$keywordsSelectorData = ['project_id' => $projectId]; // массив с параметрами запроса
 	
-	// id региона можно узнать запросом get/mod_common/regions
-	$keywordsSelectorMoscowFields = ['name', "volume:213:0:6"]; // Запрашиваемые поля. id региона Москва - 213
-	$keywordsSelectorSPbFields = ['name', "volume:2:0:6"]; // id региона Санкт-Петербург - 2
-	$keywordsSelectorRussiaFields = ['name', "volume:225:0:6"]; // id региона Россия - 225
+	// Запришиваемые поля. id Санкт-Петербурга - 2, Москвы - 213, России - 225.
+	// id региона можно узнать, выполнив запрос get/mod_common/regions
+	$keywordsSelectorFields = ['name', "volume:2:0:6", "volume:213:0:6", "volume:225:0:6"];
 	$keywordsSelectorFilter = [TV\Fields::genFilterData('name', 'EQUALS', ['топвизор'])]; // массив с указанием фильтра
 	
 	// объект для построения запроса на получение данных: https://topvisor.ru/api/v2/sdk-php/pen/
 	$keywordsSelector = new TV\Pen($TVSession, 'get', 'keywords_2', 'keywords');
 	$keywordsSelector->setData($keywordsSelectorData);
-	
-	// получаем данные для Москвы
-	$keywordsSelector->setFields($keywordsSelectorMoscowFields); // https://topvisor.ru/api/v2/basic-params/fields/
+	$keywordsSelector->setFields($keywordsSelectorFields); // https://topvisor.ru/api/v2/basic-params/fields/
 	$keywordsSelector->setFilters($keywordsSelectorFilter); // https://topvisor.ru/api/v2/basic-params/filters/
 	$pageOfKeywordsSelector = $keywordsSelector->exec(); // выполнить обращение к API
 	
@@ -41,30 +38,10 @@ try{
 	
 	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult(); // результат выполнения запроса, массив выбранных ключевых слов
 	$selectedKeyword = $resultOfKeywordsSelector[0];
-	$frequency = $selectedKeyword->{'volume:213:0:6'};
-	echo "Москва: \"$selectedKeyword->name\" - $frequency <br>\n";
-	
-	// получаем данные для Санкт-петербурга
-	$keywordsSelector->setFields($keywordsSelectorSPbFields);
-	$pageOfKeywordsSelector = $keywordsSelector->exec();
-	
-	if($pageOfKeywordsSelector->getErrors()) throw new \Exception($pageOfKeywordsSelector->getErrorsString());
-	
-	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult();
-	$selectedKeyword = $resultOfKeywordsSelector[0];
-	$frequency = $selectedKeyword->{'volume:2:0:6'};
-	echo "Санкт-Петербург: \"$selectedKeyword->name\" - $frequency<br>\n";
-	
-	// получаем данные для России
-	$keywordsSelector->setFields($keywordsSelectorRussiaFields);
-	$pageOfKeywordsSelector = $keywordsSelector->exec();
-	
-	if($pageOfKeywordsSelector->getErrors()) throw new \Exception($pageOfKeywordsSelector->getErrorsString());
-	
-	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult();
-	$selectedKeyword = $resultOfKeywordsSelector[0];
-	$frequency = $selectedKeyword->{'volume:225:0:6'};
-	echo "Россия: \"$selectedKeyword->name\" - $frequency<br>\n";
+	$frequencySPb = $selectedKeyword->{'volume:2:0:6'};
+	$frequencyMsk = $selectedKeyword->{'volume:213:0:6'};
+	$frequencyRF = $selectedKeyword->{'volume:225:0:6'};
+	echo "топвизор $frequencySPb / $frequencyMsk / $frequencyRF<br>\n";
 }catch(Exception $e){
 	echo $e->getMessage();
 }
