4ac8517290fb64603edd5f9b10e2411616874641
“uliana” <avdeewa.ul@yandex.ru>
2018-03-01 16:52:18 +0300

    no commit message

--- examples/first-step/qualifiers.php
+++ examples/first-step/qualifiers.php
@@ -17,37 +17,35 @@ $TVSession = new TV\Session(); // создание сессии: https://topviso
 
 $projectId = 2121417; // введите id своего проекта
 
+// id региона можно узнать, выполнив запрос get/mod_common/regions
+$spbRegionId = 2; // id Санкт-Петербурга
+$mskRegionId = 213; // id Москвы
+$rfRegionId = 225; // id России
+$searcherKey = 0; // ключ ПС Яндекс
+$volumeType = 6; // форма частоты "[!Ч]"
+
 try{
-	echo "<b>====== Санкт-Петербург / Москва / Россия</b><br>\n"; // вывод шапки таблицы
-	
-	$keywordsSelectorData = ['project_id' => $projectId]; // массив с параметрами запроса
-	
-	// id региона можно узнать, выполнив запрос get/mod_common/regions
-	$spbRegionId = 2; // id Санкт-Петербурга
-	$mskRegionId = 213; // id Москвы
-	$rfRegionId = 225; // id России
-	$searcherKey = 0; // ключ ПС Яндекс
-	$volumeType = 6; // форма частоты "[!Ч]"
-	
 	// описание поля volume и других полей можно посмотреть тут: https://topvisor.ru/api/v2/fields/
 	$spbVolumeField = "volume:$spbRegionId:$searcherKey:$volumeType";
 	$mskVolumeField = "volume:$mskRegionId:$searcherKey:$volumeType";
 	$rfVolumeField = "volume:$rfRegionId:$searcherKey:$volumeType";
-	
+
+	$keywordsSelectorData = ['project_id' => $projectId]; // массив с параметрами запроса
 	$keywordsSelectorFields = ['name', $spbVolumeField, $mskVolumeField, $rfVolumeField];
 	$keywordsSelectorFilter = [TV\Fields::genFilterData('name', 'EQUALS', ['топвизор'])]; // массив с указанием фильтра
-	
+
 	// объект для построения запроса на получение данных: https://topvisor.ru/api/v2/sdk-php/pen/
 	$keywordsSelector = new TV\Pen($TVSession, 'get', 'keywords_2', 'keywords');
 	$keywordsSelector->setData($keywordsSelectorData);
 	$keywordsSelector->setFields($keywordsSelectorFields); // https://topvisor.ru/api/v2/basic-params/fields/
 	$keywordsSelector->setFilters($keywordsSelectorFilter); // https://topvisor.ru/api/v2/basic-params/filters/
 	$pageOfKeywordsSelector = $keywordsSelector->exec(); // выполнить обращение к API
-	
+
 	if($pageOfKeywordsSelector->getErrors()) throw new \Exception($pageOfKeywordsSelector->getErrorsString());
-	
-	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult(); // результат выполнения запроса, массив выбранных ключевых слов
-	
+
+	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult(); // результат выполнения запроса, массив полученных ключевых слов
+
+	echo "<b>====== Санкт-Петербург / Москва / Россия</b><br>\n"; // вывод шапки таблицы
 	foreach($resultOfKeywordsSelector as $selectedKeyword){
 		echo "топвизор - $selectedKeyword->$spbVolumeField / $selectedKeyword->$mskVolumeField / $selectedKeyword->$rfVolumeField<br>\n";
 	}
