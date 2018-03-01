999ec6acb0b486d1c9b84f053518e59772e16177
“uliana” <avdeewa.ul@yandex.ru>
2018-03-01 16:11:14 +0300

    no commit message

--- examples/first-step/qualifiers.php
+++ examples/first-step/qualifiers.php
@@ -22,9 +22,18 @@ try{
 	
 	$keywordsSelectorData = ['project_id' => $projectId]; // массив с параметрами запроса
 	
-	// Запришиваемые поля. id Санкт-Петербурга - 2, Москвы - 213, России - 225.
 	// id региона можно узнать, выполнив запрос get/mod_common/regions
-	$keywordsSelectorFields = ['name', "volume:2:0:6", "volume:213:0:6", "volume:225:0:6"];
+	$spbRegionId = 2; // id Санкт-Петербурга
+	$mskRegionId = 213; // id Москвы
+	$rfRegionId = 225; // id России
+	$searcherKey = 0; // ключ ПС Яндекс
+	$frequencyType = 6; // форма частоты "[!Ч]"
+	
+	$spbVolumeField = "volume:$spbRegionId:$searcherKey:$frequencyType";
+	$mskVolumeField = "volume:$mskRegionId:$searcherKey:$frequencyType";
+	$rfVolumeField = "volume:$rfRegionId:$searcherKey:$frequencyType";
+	
+	$keywordsSelectorFields = ['name', $spbVolumeField, $mskVolumeField, $rfVolumeField];
 	$keywordsSelectorFilter = [TV\Fields::genFilterData('name', 'EQUALS', ['топвизор'])]; // массив с указанием фильтра
 	
 	// объект для построения запроса на получение данных: https://topvisor.ru/api/v2/sdk-php/pen/
@@ -39,10 +48,10 @@ try{
 	$resultOfKeywordsSelector = $pageOfKeywordsSelector->getResult(); // результат выполнения запроса, массив выбранных ключевых слов
 	
 	foreach($resultOfKeywordsSelector as $selectedKeyword){
-		$frequencySPb = $selectedKeyword->{'volume:2:0:6'};
-		$frequencyMsk = $selectedKeyword->{'volume:213:0:6'};
-		$frequencyRF = $selectedKeyword->{'volume:225:0:6'};
-		echo "топвизор - $frequencySPb / $frequencyMsk / $frequencyRF<br>\n";
+		$volumeSPb = $selectedKeyword->$spbVolumeField;
+		$volumeMsk = $selectedKeyword->$mskVolumeField;
+		$volumeRF = $selectedKeyword->$rfVolumeField;
+		echo "топвизор - $volumeSPb / $volumeMsk / $volumeRF<br>\n";
 	}
 }catch(Exception $e){
 	echo $e->getMessage();
